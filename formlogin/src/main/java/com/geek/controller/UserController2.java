28ee71df37f8657b649a3cf5447f3270345dff11
donghuihui <1060487361@qq.com>
2022-05-21 18:37:10 +0800

    登录用户数据获取，通过HttpServletRequest从当前对象中获取

--- formlogin/src/main/java/com/geek/controller/UserController2.java
+++ formlogin/src/main/java/com/geek/controller/UserController2.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
 
 @RestController
@@ -16,5 +17,15 @@ public class UserController2 {
     @RequestMapping("/principal")
     public void pricipal(Principal principal){
         System.out.println("principal = " + principal);
+    }
+
+    @RequestMapping("/info")
+    public void info(HttpServletRequest request){
+        String remoteUser  = request.getRemoteUser();
+        Authentication authentication = ((Authentication) request.getUserPrincipal());
+        boolean admin = request.isUserInRole("admin");
+        System.out.println("remoteUser = " + remoteUser);
+        System.out.println("auth.getName () = " + authentication.getName());
+        System.out.println("admin = " + admin);
     }
 }
