b3ee4ead65fa714030bc9f1ab159db25b6e75154
donghuihui <1060487361@qq.com>
2022-05-22 20:11:29 +0800

    基于Mybatis的登录用户配置文件

---
+++ mybatis/src/main/java/com/geek/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.geek.config;
+
+import com.geek.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+        auth.userDetailsService(myUserDetailsService);
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/mylogin.html")
+                .loginProcessingUrl("/doLogin")
+                .defaultSuccessUrl("/index.html")
+                .failureHandler(new MyAuthenticationFailureHandler())
+                .usernameParameter("uname")
+                .passwordParameter("passwd")
+                .permitAll()
+                .and()
+                .csrf().disable();
+    }
+}
