94f7099bcfe91977e92c114f8c4acc4e23786b58
donghuihui <1060487361@qq.com>
2022-05-24 22:23:11 +0800

    自定义全局AuthenticationManager用户

---
+++ multUser3-custom/src/main/java/com/geek/MultUser3CustomApplication.java
@@ -0,0 +1,13 @@
+package com.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultUser3CustomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultUser3CustomApplication.class, args);
+    }
+
+}
