28dc629e28440e8dfa9024c2be2463c5191836ba
donghuihui <1060487361@qq.com>
2022-05-24 22:03:14 +0800

    针对局部AuthenticationManager定义的用户

---
+++ multUser1-part/src/main/java/com/geek/MultUser1PartApplication.java
@@ -0,0 +1,13 @@
+package com.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultUser1PartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultUser1PartApplication.class, args);
+    }
+
+}
---
+++ multUser1-part/src/main/java/com/geek/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.geek;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.ObjectPostProcessor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        InMemoryUserDetailsManager users = new InMemoryUserDetailsManager();
+        users.createUser(User.withUsername("javaboy").password("{noop}123").roles("admin").build());
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .userDetailsService(users)
+                .csrf().disable();
+    }
+}
