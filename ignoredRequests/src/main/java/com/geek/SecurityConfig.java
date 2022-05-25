e926ff46d077283e534a81e166580043f276d855
donghuihui <1060487361@qq.com>
2022-05-25 19:53:06 +0800

    静态资源过滤

---
+++ ignoredRequests/src/main/java/com/geek/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.geek;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(WebSecurity web) throws Exception{
+        web.ignoring().antMatchers("/login.html","/css/**","/js/**","/image/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .csrf().disable();
+    }
+}
