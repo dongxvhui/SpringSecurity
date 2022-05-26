7f384efeccc14f7c68bc3b1673e7cc22c84d411f
donghuihui <1060487361@qq.com>
2022-05-26 23:37:54 +0800

    会话失效后，返回JSON提示

--- session01/src/main/java/com/geek/config/SecurityConfig.java
+++ session01/src/main/java/com/geek/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.geek.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -8,6 +9,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
+
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -34,7 +39,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf().disable()
                 .sessionManagement()
-                .maximumSessions(1);
+                .maximumSessions(1)
+//                .expiredUrl("/login"); //会话失效后重定向到/login
+                .expiredSessionStrategy(event -> {
+                    HttpServletResponse response = event.getResponse();
+                    response.setContentType("application/json;charset=utf-8");
+                    Map<String, Object> result = new HashMap<>();
+                    result.put("status",500);
+                    result.put("msg", "当前会话已经失效，请重新登录");
+                    String s = new ObjectMapper().writeValueAsString(result);
+                    response.getWriter().print(s);
+                    response.flushBuffer();
+                });
+
+
     }
 
     @Bean
