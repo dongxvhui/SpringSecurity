4bae8baa212003ff195d9eb8f6c5de23a8e2bd09
donghuihui <1060487361@qq.com>
2022-05-28 14:19:03 +0800

    csrf跨域攻击演示

---
+++ csrf-demo1/src/main/java/com/geek/CsrfDemo1Application.java
@@ -0,0 +1,13 @@
+package com.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsrfDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsrfDemo1Application.class, args);
+    }
+
+}
