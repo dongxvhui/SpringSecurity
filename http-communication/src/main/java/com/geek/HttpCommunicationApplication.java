ad1427fdf6d0b872136289a638085adbbaffdbdf
donghuihui <1060487361@qq.com>
2022-05-30 22:14:48 +0800

    http通信安全，使用https替代http

---
+++ http-communication/src/main/java/com/geek/HttpCommunicationApplication.java
@@ -0,0 +1,13 @@
+package com.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpCommunicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpCommunicationApplication.class, args);
+    }
+
+}
