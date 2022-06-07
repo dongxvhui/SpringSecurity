77d99da8325a592410677c3b6b179d2d2ac8ded7
donghuihui <1060487361@qq.com>
2022-06-07 22:09:14 +0800

    基于URL地址的权限管理，基本用法,角色继承

--- based-on-url/src/main/java/com/geek/SecurityConfig.java
+++ based-on-url/src/main/java/com/geek/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.geek;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    RoleHierarchy roleHierarchy(){
+        RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+        hierarchy.setHierarchy("ROLE_ADMIN > ROLE_USER");
+        return hierarchy;
+    }
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
@@ -18,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .password("{noop}123")
                 .roles("USER")
                 .and()
-                .withUser("小魔王")
+                .withUser("tom")
                 .password("{noop}123")
                 .roles("READ_INFO");
     }
@@ -27,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .antMatchers("/user/**").access("hasAnyRole('USER','ADMIN')")
+                .antMatchers("/user/**").access("hasAnyRole('USER')")
                 .antMatchers("/getinfo").hasAuthority("READ_INFO")
                 .anyRequest().access("isAuthenticated()")
                 .and()
