0fbebb512a7fa6435f89ad7e9f6280e7b63cb66e
donghuihui <1060487361@qq.com>
2022-06-11 15:43:49 +0800

    OAuth2-github初体验

---
+++ OAuth2_github/src/main/java/com/geek/OAuth2GithubApplication.java
@@ -0,0 +1,13 @@
+package com.geek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuth2GithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2GithubApplication.class, args);
+    }
+
+}
